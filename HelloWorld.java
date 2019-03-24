9347a21bba2144a93158a9fb7fc11aec66575d18
Luis Cornejo <luis.cornejo.96@hotmail.com>
2019-03-24 17:24:21 -0600

    Cambio fallos Piramide *

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,9 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 public class HelloWorld{
     public static void main(String [] kiwi){
         System.out.println("Hello World in Java");
-        System.out.println("Primer cambio en Jenkins Auto-Detectable");
+        System.out.println("Segundo cambio en Jenkins Auto-Detectable");
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < i; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println("");
