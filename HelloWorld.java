118deaeee647d13cd4b8a2967ecf461141ea1b11
Luis Cornejo <luis.cornejo.96@hotmail.com>
2019-03-24 17:16:05 -0600

    Cambio para que Jenkins lo reconozca

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String [] kiwi){
         System.out.println("Hello World in Java");
+        System.out.println("Primer cambio en Jenkins Auto-Detectable");
     }
 }
