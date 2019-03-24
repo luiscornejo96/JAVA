469e889c2a99bbfa1c08cade75d80ca258142af0
Luis Cornejo <luis.cornejo.96@hotmail.com>
2019-03-24 17:21:18 -0600

    Matriz de puntos agregada para 2º Cambio en Jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,14 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class HelloWorld{
     public static void main(String [] kiwi){
         System.out.println("Hello World in Java");
         System.out.println("Primer cambio en Jenkins Auto-Detectable");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; i++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
