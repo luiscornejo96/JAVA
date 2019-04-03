d90fe2d10eb551a4eb42a665050a54cc9cbe92fd
Luis Cornejo <luis.cornejo.96@hotmail.com>
2019-04-02 19:01:12 -0600

    Prueba redes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 public class HelloWorld{
     public static void main(String [] kiwi){
         System.out.println("Hello World in Java");
-        System.out.println("Segundo cambio en Jenkins Auto-Detectable");
+        System.out.println("Prueba");
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.print("**");
+                System.out.print("*");
             }
             System.out.println("");
         }
